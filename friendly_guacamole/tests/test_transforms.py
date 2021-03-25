8d965262261b503a2ec7f99d4f7766bcdf67655e
George Armstrong <garmstrong@colgate.edu>
2021-03-25 14:21:34 -0700

    TST at tests for center log ratio

--- friendly_guacamole/tests/test_transforms.py
+++ friendly_guacamole/tests/test_transforms.py
@@ -1,9 +1,10 @@
 from unittest import TestCase
 from biom import Table
-from friendly_guacamole.transforms import AsDense
+from friendly_guacamole.transforms import AsDense, CLR
 import pandas as pd
 import numpy as np
 from pandas.testing import assert_frame_equal
+from numpy.testing import assert_array_almost_equal
 
 
 class AsDenseTestCase(TestCase):
@@ -29,3 +30,31 @@ class AsDenseTestCase(TestCase):
             index=['s1', 's2', 's3'],
         )
         assert_frame_equal(expected, out)
+
+
+class CLRTests(TestCase):
+    def setUp(self):
+        self.table = np.array([
+            [1., 2., 3.],
+            [0., 7., 0.],
+        ])
+
+    def test_clr_transform(self):
+        t = CLR()
+        X = t.fit_transform(self.table)
+        g = 2 * (3 ** (1 / 3))
+        expected = np.array([
+            [np.log(2 / g), np.log(3 / g), np.log(4 / g), ],
+            [np.log(1 / 2), np.log(8 / 2), np.log(1 / 2), ],
+        ])
+        assert_array_almost_equal(expected, X)
+
+    def test_clr_transform_pandas(self):
+        t = CLR()
+        X = t.fit_transform(pd.DataFrame(self.table))
+        g = 2 * (3 ** (1 / 3))
+        expected = np.array([
+            [np.log(2 / g), np.log(3 / g), np.log(4 / g), ],
+            [np.log(1 / 2), np.log(8 / 2), np.log(1 / 2), ],
+        ])
+        assert_array_almost_equal(expected, X)
