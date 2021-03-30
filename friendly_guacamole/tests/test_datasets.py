728177fe444ad3784cd41965de73bd5622f56ee7
George Armstrong <garmstrong@colgate.edu>
2021-03-30 16:40:10 -0700

    add tests for existing data sets

--- friendly_guacamole/tests/test_datasets.py
+++ friendly_guacamole/tests/test_datasets.py
@@ -1,8 +1,11 @@
 from unittest import TestCase
+from tempfile import TemporaryDirectory
 from friendly_guacamole.datasets import (
     Dataset,
     FileSystemArchiver,
     QiitaClient,
+    KeyboardDataset,
+    DietInterventionStudy,
 )
 
 
@@ -97,3 +100,19 @@ class DatasetTestCase(TestCase):
         self.assertEqual(dataset['baz'], 'qux')
         with self.assertRaises(KeyError):
             dataset['qux']
+
+
+class TestRealDataSets(TestCase):
+    def test_keyboard(self):
+        with TemporaryDirectory() as td:
+            kbd = KeyboardDataset(td)
+            kbd['table']
+            kbd['metadata']
+            kbd['tree']
+
+    def test_diet(self):
+        with TemporaryDirectory() as td:
+            dis = DietInterventionStudy(td)
+            dis['table']
+            dis['metadata']
+            dis['tree']
