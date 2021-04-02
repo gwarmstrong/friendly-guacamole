0bfbb87ae1a66aa5b9ab5df955db719a894a4368
George Armstrong <garmstrong@colgate.edu>
2021-04-02 11:06:09 -0700

    remove old datasets

--- friendly_guacamole/datasets.py
+++ friendly_guacamole/datasets.py
@@ -14,234 +14,8 @@ from friendly_guacamole.exceptions import DatasetError
 from http.client import HTTPResponse
 
 
-def download_and_extract_response(response, path, unzip=True):
-    zip_path = path
-    if unzip:
-        os.makedirs(path, exist_ok=True)
-        zip_path = path + '.zip'
-    else:
-        pardir = os.path.abspath(os.path.join(path, os.pardir))
-        os.makedirs(pardir, exist_ok=True)
-
-    with open(zip_path, 'wb') as fp:
-        fp.write(response.read())
-
-    if unzip:
-        with ZipFile(zip_path, 'r') as fp:
-            fp.extractall(path)
-        os.remove(zip_path)
-
-
-class ArchiverMixin:
-    dataset: 'Dataset'
-    save: Callable
-
-
-class QiitaSaveMixin(ArchiverMixin):
-
-    def _download_method(self, response, path, unzip=True):
-        download_and_extract_response(response, path, unzip=unzip)
-
-    def save(self, artifact: 'Artifact', response: HTTPResponse, unzip=True):
-        path = os.path.join(self.dataset.path, artifact.name)
-        self._download_method(response, path, unzip=unzip)
-
-
-class FileSystemArchiver(QiitaSaveMixin):
-
-    def __init__(self, dataset):
-        self.dataset: Dataset = dataset
-        self.path = self.dataset.path
-
-    def read(self, artifact: 'FileSystemArtifact'):
-        return artifact.filesystem_read(self)
-
-    def exists(self, artifact: 'FileSystemArtifact'):
-        return artifact.filesystem_exists(self)
-
-    def path(self, artifact: 'FileSystemArtifact'):
-        return artifact.filesystem_path(self)
-
-
-class Artifact:
-    name: str
-
-    @staticmethod
-    def merge(self, other):
-        raise NotImplementedError()
-
-
-class QiitaArtifact(Artifact):
-    _artifact_fstring = "https://qiita.ucsd.edu/public_artifact_download/" \
-                        "?artifact_id={}"
-    _metadata_fstring = "https://qiita.ucsd.edu/public_download/" \
-                        "?data=sample_information&study_id={}"
-
-    def qiita_download(self, client: 'QiitaClient', archiver: ArchiverMixin):
-        raise NotImplementedError('You should implement this.')
-
-
-class FileSystemArtifact(Artifact):
-    def filesystem_exists(self, archiver: FileSystemArchiver):
-        raise NotImplementedError('You should implement this.')
-
-    def filesystem_read(self, archiver: FileSystemArchiver):
-        raise NotImplementedError('You should implement this.')
-
-    def filesystem_path(self, archiver: FileSystemArchiver):
-        raise NotImplementedError('You should implement this.')
-
-
-class Table(QiitaArtifact, FileSystemArtifact):
-    name = 'table'
-
-    def __init__(self, artifact_id):
-        self.artifact_id = artifact_id
-
-    def qiita_download(self, client: 'QiitaClient', archiver: ArchiverMixin):
-        table_link = self._artifact_fstring.format(self.artifact_id)
-        r = client.make_request(table_link)
-        archiver.save(self, r)
-
-    def filesystem_exists(self, archiver: FileSystemArchiver):
-        path = self.filesystem_path(archiver)
-        return os.path.exists(path)
-
-    def filesystem_read(self, archiver: FileSystemArchiver):
-        path = self.filesystem_path(archiver)
-        return load_table(path)
-
-    def filesystem_path(self, archiver: FileSystemArchiver):
-        path_to_table = os.path.join(
-            archiver.path, self.name, 'BIOM',
-            str(self.artifact_id),
-            'otu_table.biom',
-        )
-        return os.path.abspath(path_to_table)
-
-    @staticmethod
-    def merge(t1, t2):
-        return t1.merge(t2)
-
-
-class ArtifactList(QiitaArtifact, FileSystemArtifact):
-
-    def __init__(self, *artifacts):
-        self.artifacts = list(artifacts)
-
-    def qiita_download(self, client: 'QiitaClient', archiver: ArchiverMixin):
-        for artifact in self.artifacts:
-            artifact.qiita_download(client, archiver)
-
-    def filesystem_exists(self, archiver: FileSystemArchiver):
-        return all(
-            artifact.filesystem_exists(archiver) for artifact in self.artifacts
-        )
-
-    def filesystem_read(self, archiver: FileSystemArchiver):
-        if len(self.artifacts) > 0:
-            artifact = self.artifacts[0]
-            merged = artifact.filesystem_read(archiver)
-        for artifact in self.artifacts[1:]:
-            other = artifact.filesystem_read(archiver)
-            merged = artifact.merge(merged, other)
-        return merged
-
-
-class Tables(ArtifactList):
-    name = 'table'
-
-
-class GenericWebArtifact(QiitaArtifact, FileSystemArtifact):
-    link: str
-
-    def __init__(self):
-        self.path = None
-
-    def qiita_download(self, client: 'QiitaClient', archiver: ArchiverMixin):
-        r = client.make_request(self.link)
-        archiver.save(self, r, unzip=False)
-
-    def filesystem_exists(self, archiver):
-        # being able to use path depends on datasets being checked for
-        # existence when the dataset is instantiated
-        self.path = self.filesystem_path(archiver)
-        return os.path.exists(self.path)
-
-    def filesystem_path(self, archiver):
-        path = os.path.join(
-            archiver.path, self.name
-        )
-        return os.path.abspath(path)
-
-
-class GG97OTUsTree(GenericWebArtifact):
-    name = '97_otus.tree'
-    link = 'ftp://greengenes.microbio.me/greengenes_release/gg_13_8_otus/' \
-           'trees/97_otus.tree'
-
-    def filesystem_read(self, archiver: FileSystemArchiver):
-        return self
-
-
-class Metadata(QiitaArtifact, FileSystemArtifact):
-    name = 'metadata'
-
-    def __init__(self, study_id):
-        self.study_id = study_id
-
-    def qiita_download(self, client: 'QiitaClient', archiver: ArchiverMixin):
-        metadata_link = self._metadata_fstring.format(
-            self.study_id)
-        r = client.make_request(metadata_link)
-        archiver.save(self, r)
-
-    def filesystem_exists(self, archiver):
-        return True if self.filesystem_path(archiver) else False
-
-    def filesystem_read(self, archiver):
-        path = self.filesystem_path(archiver)
-        return pd.read_csv(path, sep='\t')
-
-    def filesystem_path(self, archiver):
-        path_to_metadata = os.path.join(
-            archiver.path, self.name, 'templates'
-        )
-        if not os.path.exists(path_to_metadata):
-            return False
-        md_candidates = list(
-            filter(lambda n: n.endswith('.txt'),
-                   os.listdir(path_to_metadata)
-                   )
-        )
-        if len(md_candidates) == 0:
-            return False
-        else:
-            md_file = md_candidates[0]
-        full_path = os.path.join(path_to_metadata, md_file)
-        return os.path.abspath(full_path)
-
-
-class QiitaClient:
-    _artifact_fstring = "https://qiita.ucsd.edu/public_artifact_download/" \
-                        "?artifact_id={}"
-    _metadata_fstring = "https://qiita.ucsd.edu/public_download/" \
-                        "?data=sample_information&study_id={}"
-
-    def __init__(self, dataset, archiver: QiitaSaveMixin):
-        self.dataset: Dataset = dataset
-        self.archiver = archiver
-
-    def download(self, artifact: QiitaArtifact):
-        artifact.qiita_download(self, self.archiver)
-
-    @staticmethod
-    def make_request(url):
-        return request.urlopen(url)
-
-
-class NewDataset(ABC):
-    artifacts: Dict[str, Artifact] = dict()
+class Dataset(ABC):
+    artifacts: Dict[str, 'ArtifactInterface'] = dict()
 
     def __init__(self, path, download=True):
         self.path = path
@@ -557,68 +331,10 @@ class GreenGenes97Tree(ArtifactInterface):
             tree = self.archiver.read(dataset.path)
             self._tree_file.write(tree)
             self._tree_file.flush()
-        return self._tree_file
-
-
-class Dataset(ABC):
-    artifacts: Dict[str, Artifact] = dict()
-    archiver_type = FileSystemArchiver
-    client_type = QiitaClient
-
-    def __init__(self, path, download=True):
-        self.path = path
-        self.archiver = self.archiver_type(self)
-        self.client = self.client_type(self, self.archiver)
-
-        if download:
-            self.download()
-
-        if not self._check_integrity():
-            raise DatasetError('Dataset not found or corrupted.' +
-                               ' You can use download=True to download it')
-
-        self._table = None
-        self._metadata = None
-        self._data = dict()
-
-    def download(self):
-        if self._check_integrity():
-            print('Files already downloaded and verified')
-            return
-        print('Downloading...', end='')
-        for artifact in self.artifacts.values():
-            self.client.download(artifact)
-        print(' Done.')
-
-    def _check_integrity(self):
-        return all(
-            self.archiver.exists(artifact) for artifact in
-            self.artifacts.values()
-        )
-
-    def __getitem__(self, item):
-        if item in self._data:
-            return self._data[item]
-        elif item in self.artifacts:
-            value = self.archiver.read(self.artifacts[item])
-            self._data[item] = value
-            return value
-        else:
-            raise KeyError(item)
+        return self._tree_file.name
 
 
 class KeyboardDataset(Dataset):
-    study_id = 232
-    table_artifact_id = 46809
-
-    artifacts = {
-        'metadata': Metadata(study_id),
-        'table': Table(table_artifact_id),
-        'tree': GG97OTUsTree(),
-    }
-
-
-class NewKeyboardDataset(NewDataset):
     study_id = 232
     table_artifact_id = 46809
 
@@ -634,27 +350,13 @@ class DietInterventionStudy(Dataset):
     table_artifact_ids = [63512, 63515]
 
     artifacts = {
-        'metadata': Metadata(study_id),
-        'table': Tables(
-            Table(table_artifact_ids[0]),
-            Table(table_artifact_ids[1]),
-        ),
-        'tree': GG97OTUsTree(),
-    }
-
-
-class NewDietInterventionStudy(NewDataset):
-    study_id = 11550
-    table_artifact_ids = [63512, 63515]
-
-    artifacts = {
         'metadata': QiitaMetadata(study_id),
         'table': TableList(table_artifact_ids),
         'tree': GreenGenes97Tree(),
     }
 
 
-class HMPV13V35(NewDataset):
+class HMPV13V35(Dataset):
 
     study_ids = [1927, 1928]
     table_artifact_ids = [47414, 47420]
